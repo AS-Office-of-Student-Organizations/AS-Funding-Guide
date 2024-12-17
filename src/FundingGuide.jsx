22c59a33eb2e0961acfb05b952c6823f6be0ea85
kabir-vats <kabir.vats.2004@gmail.com>
2024-12-16 18:18:10 -0800

    Create & Link Firestore database

---
+++ src/FundingGuide.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+
+function FundingGuide() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchFundingGuide = async () => {
+      const querySnapshot = await getDocs(collection(db, "funding-step"));
+      console.log(querySnapshot);
+      const items = querySnapshot.docs.map(doc => doc.data());
+      setData(items);
+    };
+    fetchFundingGuide();
+  }, []);
+
+  return (
+    <div>
+      <h1>Funding Guide</h1>
+      {data.map((item, index) => (
+        <div key={index}>
+          <h3>{item.content}</h3>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FundingGuide;
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxXj1VEE1ZEcVQqq3cSFANM42V9WtTGUs",
+  authDomain: "as-funding-guide.firebaseapp.com",
+  projectId: "as-funding-guide",
+  storageBucket: "as-funding-guide.firebasestorage.app",
+  messagingSenderId: "1006590852682",
+  appId: "1:1006590852682:web:a7363b77d534a02fd9f0b3",
+  measurementId: "G-C0WQ633EXL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);   
+const analytics = getAnalytics(app);
