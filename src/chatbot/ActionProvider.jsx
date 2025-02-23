b12f11390f3d5722d004ac7bad2ee6a8265bf141
kabir-vats <kabir.vats.2004@gmail.com>
2025-02-23 14:56:21 -0800

    added authentication headers to chatbot requests

--- src/chatbot/ActionProvider.jsx
+++ src/chatbot/ActionProvider.jsx
@@ -1,47 +1,67 @@
-// ActionProvider starter code
-//test
-class ActionProvider {
-    constructor(
-     createChatBotMessage,
-     setStateFunc,
-     createClientMessage,
-     stateRef,
-     createCustomMessage,
-     ...rest
-   ) {
-     this.createChatBotMessage = createChatBotMessage;
-     this.setState = setStateFunc;
-     this.createClientMessage = createClientMessage;
-     this.stateRef = stateRef;
-     this.createCustomMessage = createCustomMessage;
-   }
-   talk = (message) => {
-    fetch("http://127.0.0.1:8000/query", { // change to deployed IP
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: message }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const botMessage = this.createChatBotMessage(data.response);
-        this.setState((prev) => ({
-          ...prev,
-          messages: [...prev.messages, botMessage],
-        }));
-      })
-      .catch((error) => {
-        const errorMessage = this.createChatBotMessage(
-          "Sorry, something went wrong."
-        );
-        this.setState((prev) => ({
-          ...prev,
-          messages: [...prev.messages, errorMessage],
-        }));
-      });
- }
-}
- 
+import { getAuth } from "firebase/auth";
 
- export default ActionProvider;
+class ActionProvider {
+  constructor(
+    createChatBotMessage,
+    setStateFunc,
+    createClientMessage,
+    stateRef,
+    createCustomMessage,
+    ...rest
+  ) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+    this.createClientMessage = createClientMessage;
+    this.stateRef = stateRef;
+    this.createCustomMessage = createCustomMessage;
+  }
+
+  talk = async (message) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      const errorMessage = this.createChatBotMessage("You must be logged in.");
+      this.setState((prev) => ({
+        ...prev,
+        messages: [...prev.messages, errorMessage],
+      }));
+      return;
+    }
+
+    try {
+      const token = await user.getIdToken(); // 🔹 Fetch Firebase token
+
+      const response = await fetch("http://127.0.0.1:8000/query", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // 🔹 Include Firebase token
+        },
+        body: JSON.stringify({ question: message }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Server error");
+      }
+
+      const data = await response.json();
+      const botMessage = this.createChatBotMessage(data.response);
+      this.setState((prev) => ({
+        ...prev,
+        messages: [...prev.messages, botMessage],
+      }));
+    } catch (error) {
+      console.error("Error:", error);
+      const errorMessage = this.createChatBotMessage(
+        "Sorry, authentication failed or something went wrong."
+      );
+      this.setState((prev) => ({
+        ...prev,
+        messages: [...prev.messages, errorMessage],
+      }));
+    }
+  };
+}
+
+export default ActionProvider;
