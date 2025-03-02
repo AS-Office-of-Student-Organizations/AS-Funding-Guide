0808d039b04e7ad6fc4e4b5a30ed8c9ee5795304
kabir-vats <kabir.vats.2004@gmail.com>
2025-03-01 18:47:47 -0800

    massive linting

--- src/components/DeadlineDatePicker.jsx
+++ src/components/DeadlineDatePicker.jsx
@@ -1,37 +1,45 @@
-import {Button, Calendar, CalendarCell, CalendarGrid, DateInput, DatePicker, DateSegment, Dialog, Group, Heading, Popover} from 'react-aria-components';
+import React from 'react';
+import {
+  Button,
+  Calendar,
+  CalendarCell,
+  CalendarGrid,
+  DateInput,
+  DatePicker,
+  DateSegment,
+  Dialog,
+  Group,
+  Heading,
+  Popover,
+} from 'react-aria-components';
 import PropTypes from 'prop-types';
 
-const DeadlineDatePicker = ({value, onChange}) => {
-    
-    return (
-        <DatePicker defaultValue={value} onChange={onChange} aria-labelledby='Date'>
-        <Group>
-            <DateInput>
-            {(segment) => <DateSegment segment={segment} />}
-            </DateInput>
-            <Button>▼</Button>
-        </Group>
-        <Popover>
-            <Dialog>
-            <Calendar>
-                <header>
-                <Button slot="previous">◀</Button>
-                <Heading />
-                <Button slot="next">▶</Button>
-                </header>
-                <CalendarGrid>
-                {(date) => <CalendarCell date={date} />}
-                </CalendarGrid>
-            </Calendar>
-            </Dialog>
-        </Popover>
-        </DatePicker>
-    )
-}
+const DeadlineDatePicker = ({ value, onChange }) => {
+  return (
+    <DatePicker defaultValue={value} onChange={onChange} aria-labelledby="Date">
+      <Group>
+        <DateInput>{segment => <DateSegment segment={segment} />}</DateInput>
+        <Button>▼</Button>
+      </Group>
+      <Popover>
+        <Dialog>
+          <Calendar>
+            <header>
+              <Button slot="previous">◀</Button>
+              <Heading />
+              <Button slot="next">▶</Button>
+            </header>
+            <CalendarGrid>{date => <CalendarCell date={date} />}</CalendarGrid>
+          </Calendar>
+        </Dialog>
+      </Popover>
+    </DatePicker>
+  );
+};
 
-export default DeadlineDatePicker
+export default DeadlineDatePicker;
 
 DeadlineDatePicker.propTypes = {
-    value: PropTypes.string,    
-    onChange: PropTypes.func,    
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
--- src/guide/Guide.jsx
+++ src/guide/Guide.jsx
@@ -1,52 +1,49 @@
-import  { useEffect, useState } from "react";
-import { Route, Routes, Navigate} from "react-router-dom";
-import { db } from "@/components/firebase.jsx"; 
-import { getDoc, doc} from "firebase/firestore";
-import GuideSideBar from "./components/GuideSideBar.jsx";
-import GuidePage from "./components/GuidePage.jsx";
-import "./Guide.css";
-
+import { useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { db } from '@/components/firebase.jsx';
+import { getDoc, doc } from 'firebase/firestore';
+import GuideSideBar from './components/GuideSideBar.jsx';
+import GuidePage from './components/GuidePage.jsx';
+import './Guide.css';
 
 const Guide = () => {
-    const [pages, setPages] = useState([]); // Initialize state for pages
-  
-    useEffect(() => {
-      const fetchPages = async () => {
-        try {
-            const docRef = doc(db, "public", "guide"); // hard code for now
-            const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                setPages(docSnap.data().pages);
-            } else {
-                console.error("no such document!");
-            }
-        } catch (error) {
-            console.error("Error fetching pages:", error);
+  const [pages, setPages] = useState([]); // Initialize state for pages
+
+  useEffect(() => {
+    const fetchPages = async () => {
+      try {
+        const docRef = doc(db, 'public', 'guide'); // hard code for now
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setPages(docSnap.data().pages);
+        } else {
+          console.error('no such document!');
         }
-      };
-  
-      fetchPages();
-    }, []);
-  
-    return (
-        <div className="guide">
-            <GuideSideBar pages={pages} />
-            <Routes>
-                {pages.map((page, index) => (
-                <Route
-                    key={index}
-                    path={`/${page.pageName.toLowerCase().replace(/\s+/g, "-")}`}
-                    element={<GuidePage content={page.pageContent} edit={false} />}
-                />
-                ))}
-                <Route path="/" 
-                    element={<Navigate to='introduction'/>}
-                />
-            </Routes>
-        </div>
-    );
-  };
-  
-  export default Guide;
-  // pages[1] &&  
-  // `/ ${pages[1].pageName.toLowerCase().replace(/\s+/g, "-")}`
+      } catch (error) {
+        console.error('Error fetching pages:', error);
+      }
+    };
+
+    fetchPages();
+  }, []);
+
+  return (
+    <div className="guide">
+      <GuideSideBar pages={pages} />
+      <Routes>
+        {pages.map((page, index) => (
+          <Route
+            key={index}
+            path={`/${page.pageName.toLowerCase().replace(/\s+/g, '-')}`}
+            element={<GuidePage content={page.pageContent} edit={false} />}
+          />
+        ))}
+        <Route path="/" element={<Navigate to="introduction" />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Guide;
+// pages[1] &&
+// `/ ${pages[1].pageName.toLowerCase().replace(/\s+/g, "-")}`
