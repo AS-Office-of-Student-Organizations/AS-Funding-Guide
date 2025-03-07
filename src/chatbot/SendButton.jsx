9a0b4877c2c5291ccd677c2eb2a667abc56709e9
kabir-vats <kabir.vats.2004@gmail.com>
2025-03-06 21:52:11 -0800

    small changes to make the icons not sticky

--- src/chatbot/SendButton.jsx
+++ src/chatbot/SendButton.jsx
@@ -1,13 +1,16 @@
-"use client"
-import { CornerDownLeft } from "lucide-react"
+'use client';
+import { CornerDownLeft } from 'lucide-react';
 
-const SendButton = (props) => {
+const SendButton = props => {
   return (
-    <button className="react-chatbot-kit-chat-btn-send" onClick={props.onClick} aria-label="Send message">
-      <CornerDownLeft size={24} strokeWidth={2.5} color="white" /> 
+    <button
+      className="react-chatbot-kit-chat-btn-send"
+      onClick={props.onClick}
+      aria-label="Send message"
+    >
+      <CornerDownLeft size={24} strokeWidth={2.5} color="white" />
     </button>
-  )
-}
+  );
+};
 
-export default SendButton
-
+export default SendButton;
--- src/chatbot/config.jsx
+++ src/chatbot/config.jsx
@@ -1,36 +1,39 @@
-"use client"
+'use client';
 
-import { createChatBotMessage } from "react-chatbot-kit"
-import { CornerDownLeft } from "lucide-react"
+import { createChatBotMessage } from 'react-chatbot-kit';
+import { CornerDownLeft } from 'lucide-react';
 
-const botName = "A.S."
+const botName = 'A.S.';
 
 const config = {
   initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
   botName: botName,
   customStyles: {
     botMessageBox: {
-      backgroundColor: "#376B7E",
+      backgroundColor: '#376B7E',
     },
   },
   customComponents: {
-    botAvatar: (props) => (
+    botAvatar: props => (
       <div className="react-chatbot-kit-chat-bot-avatar-container">
-        <span>AS</span>
+        <span></span>
       </div>
     ),
     // Define the send button directly in the config
-    sendButton: (props) => (
-      <button className="react-chatbot-kit-chat-btn-send" onClick={props.onClick} aria-label="Send message">
+    sendButton: props => (
+      <button
+        className="react-chatbot-kit-chat-btn-send"
+        onClick={props.onClick}
+        aria-label="Send message"
+      >
         <CornerDownLeft size={24} strokeWidth={2.5} color="white" />
       </button>
     ),
   },
   state: {
-    gist: "",
+    gist: '',
     infoBox: false,
   },
-}
+};
 
-export default config
-
+export default config;
